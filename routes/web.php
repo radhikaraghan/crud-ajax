fde89da143b49a6fa450beb9d5c360ebf80ff497
radhi <radhi@gmail.com>
2023-06-22 15:37:03 +0530

    register and login done

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,43 @@ use App\Models\Product;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
+});
+*/
+
+Route::group(['namespace' => 'App\Http\Controllers'], function()
+{   
+    /**
+     * Home Routes
+     */
+    Route::get('/', 'HomeController@index')->name('home.index');
+
+    Route::group(['middleware' => ['guest']], function() {
+        /**
+         * Register Routes
+         */
+        Route::get('/register', 'RegisterController@show')->name('register.show');
+        Route::post('/register', 'RegisterController@register')->name('register.perform');
+
+        /**
+         * Login Routes
+         */
+        Route::get('/login', 'LoginController@show')->name('login.show');
+        Route::post('/login', 'LoginController@login')->name('login.perform');
+
+    });
+
+    Route::group(['middleware' => ['auth']], function() {
+        /**
+         * Logout Routes
+         */
+        Route::get('/logout', 'LogoutController@perform')->name('logout.perform');
+    });
 });
 
 
-Route::get('/', [ProductController::class, 'index']);
+Route::get('/dashboard', [ProductController::class, 'index'])->name('dashboard');
 Route::post('/store', [ProductController::class, 'store'])->name('store');
 Route::get('/fetchall', [ProductController::class, 'fetchAll'])->name('fetchAll');
 Route::delete('/delete', [ProductController::class, 'delete'])->name('delete');
